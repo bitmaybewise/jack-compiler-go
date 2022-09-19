5ddefc7fdb14357f8f09b88567114c1d8c438bfb
Hercules Merscher <hlmerscher@gmail.com>
2022-09-19 23:48:00 +0200

    CompileClassVarDec

--- engine/engine.go
+++ engine/engine.go
@@ -2,47 +2,65 @@ package engine
 
 import (
 	"encoding/xml"
+	"errors"
 	"fmt"
 
 	"github.com/hlmerscher/jack-compiler-go/tokenizer"
 )
 
+var (
+	notClassVarDec = errors.New("not a class variable declaration")
+)
+
 type NestedToken struct {
 	XMLName  xml.Name
-	Children []xml.Marshaler
+	Children []any
 }
 
-func (nt *NestedToken) append(token xml.Marshaler) {
+func (nt *NestedToken) append(token any) []any {
 	if token != nil {
 		nt.Children = append(nt.Children, token)
 	}
+	return nt.Children
 }
 
 func makeNestedToken(token *tokenizer.Token) *NestedToken {
-	return &NestedToken{
-		XMLName:  xml.Name{Local: token.Raw},
-		Children: []xml.Marshaler{token},
-	}
+	return &NestedToken{XMLName: xml.Name{Local: token.Raw}}
 }
 
+var classNames = map[string]struct{}{}
+
 func CompileClass(tk *tokenizer.Tokenizer) (*NestedToken, error) {
-	token, err := processToken(tk, "class")
+	classToken, err := processToken(tk, is("class"))
 	if err != nil {
 		return nil, err
 	}
-	nestedToken := makeNestedToken(token)
+	nestedToken := makeNestedToken(classToken)
+	nestedToken.append(classToken)
 
 	termToken, err := CompileTerm(tk)
 	if err != nil {
 		return nil, err
 	}
 	nestedToken.append(termToken)
+	classNames[termToken.Raw] = struct{}{}
 
-	// varDecToken, err := CompileClassVarDec(tk)
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// nestedToken.append(varDecToken)
+	openToken, err := processToken(tk, is("{"))
+	if err != nil {
+		return nil, err
+	}
+	nestedToken.append(openToken)
+
+	for {
+		varDecToken, err := CompileClassVarDec(tk)
+		if errors.Is(err, notClassVarDec) {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		nestedToken.append(varDecToken)
+	}
 
 	// subRoutineToken, err := CompileSubroutine(tk)
 	// if err != nil {
@@ -50,13 +68,7 @@ func CompileClass(tk *tokenizer.Tokenizer) (*NestedToken, error) {
 	// }
 	// nestedToken.append(subRoutineToken)
 
-	openToken, err := processToken(tk, "{")
-	if err != nil {
-		return nil, err
-	}
-	nestedToken.append(openToken)
-
-	closeToken, err := processToken(tk, "}")
+	closeToken, err := processToken(tk, is("}"))
 	if err != nil {
 		return nil, err
 	}
@@ -79,18 +91,79 @@ func CompileTerm(tk *tokenizer.Tokenizer) (*tokenizer.Token, error) {
 	return &token, nil
 }
 
-func CompileClassVarDec(tk *tokenizer.Tokenizer) (*tokenizer.Token, error) {
-	return nil, fmt.Errorf("not implemented error")
+func CompileClassVarDec(tk *tokenizer.Tokenizer) (*NestedToken, error) {
+	classVarDecToken, err := processToken(tk, is("static"), is("field"))
+	if err != nil {
+		return nil, fmt.Errorf("%w (%s)", notClassVarDec, err)
+	}
+
+	nestedToken := makeNestedToken(&tokenizer.Token{Raw: "classDecVar"})
+	nestedToken.append(classVarDecToken)
+
+	typeToken, err := processToken(tk, is("boolean"), is("int"), is("char"), isClass())
+	if err != nil {
+		return nil, err
+	}
+	nestedToken.append(typeToken)
+
+	for {
+		termToken, err := CompileTerm(tk)
+		if err != nil {
+			return nil, err
+		}
+		nestedToken.append(termToken)
+
+		colonToken, err := processToken(tk, is(","))
+		if err != nil {
+			break
+		}
+		nestedToken.append(colonToken)
+	}
+
+	semicolonToken, err := processToken(tk, is(";"))
+	if err != nil {
+		return nil, err
+	}
+	nestedToken.append(semicolonToken)
+
+	return nestedToken, nil
 }
 
 func CompileSubroutine(tk *tokenizer.Tokenizer) (*tokenizer.Token, error) {
 	return nil, fmt.Errorf("not implemented error")
 }
 
-func processToken(tk *tokenizer.Tokenizer, expectedToken string) (*tokenizer.Token, error) {
-	if tk.Current.Raw != expectedToken {
-		return nil, fmt.Errorf("wrong token error, expected <%s>, got <%s>", expectedToken, tk.Current.Raw)
+type tokenMatcher func(tokenizer.Token) (string, bool)
+
+func is(tokenTerm string) tokenMatcher {
+	return func(t tokenizer.Token) (string, bool) {
+		return tokenTerm, t.Raw == tokenTerm
 	}
+}
+
+func isClass() tokenMatcher {
+	return func(t tokenizer.Token) (string, bool) {
+		_, ok := classNames[t.Raw]
+		return t.Raw, ok
+	}
+}
+
+func processToken(tk *tokenizer.Tokenizer, matchers ...tokenMatcher) (*tokenizer.Token, error) {
+	var expToken string
+	var tokenNames []string
+
+	for _, match := range matchers {
+		token, ok := match(tk.Current)
+		tokenNames = append(tokenNames, token)
+		if ok {
+			expToken = token
+		}
+	}
+
+	if expToken == "" {
+		return nil, fmt.Errorf("wrong token error, expected %q, got %q", tokenNames, tk.Current.Raw)
+	}
+
 	token := tk.Current
 
 	_, err := tk.Advance()
